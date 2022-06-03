0c965231dd29293ceb61599a2772402f9189b35b
Artur Saiakhov <pokermenu@gmail.com>
2022-06-03 13:50:34 +0300

    2.11-4 add OfferCard to MainScreen. Add MainnScreen to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import WelcomeScreen from './components/welcome-screen/welcome-screen';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <WelcomeScreen />
+    
   </React.StrictMode>,
   document.getElementById('root'));
