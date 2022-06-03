6a562b3b6bc214217db22b1b33c185b7ede61cc6
Artur Saiakhov <pokermenu@gmail.com>
2022-06-03 22:32:55 +0300

    add all pages and render in main screen to test it

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,27 @@
-import MainScreen from '../main-screen/main-screen';
+import MainScreen from "../main-screen/main-screen";
+import OfferPage from "../offer-page/offer-page";
+import FavoritesPage from "../favorites-page/favorites-page";
+import FavoritesPageEmpty from "../favorites-page-empty/favorites-page-empty";
+import LoginPage from "../login-page/login-page";
+import MainScreenEmpty from "../main-screen-empty/main-screen-empty";
+import OfferPageNotLogged from "../offer-page-not-logged/offer-page-not-logged";
 
 type AppScreenProps = {
   cardsCount: number;
-}
+};
 
-function App({cardsCount}: AppScreenProps): JSX.Element {
-  
-  return <MainScreen cardsCount={cardsCount} />;
+function App({ cardsCount }: AppScreenProps): JSX.Element {
+  return (
+    <>
+      <MainScreen cardsCount={cardsCount} />
+      <OfferPage />
+      <FavoritesPage />
+      <LoginPage />
+      <FavoritesPageEmpty />
+      <MainScreenEmpty />
+      <OfferPageNotLogged />
+    </>
+  );
 }
 
 export default App;
