7a271b2584da9713f320967afc3d23e91e50eccc
Artur Saiakhov <pokermenu@gmail.com>
2022-06-05 23:27:30 +0300

    add prettier config, fix problems eslint

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,6 +5,7 @@ import FavoritesPageEmpty from "../favorites-page-empty/favorites-page-empty";
 import LoginPage from "../login-page/login-page";
 import MainScreenEmpty from "../main-screen-empty/main-screen-empty";
 import OfferPageNotLogged from "../offer-page-not-logged/offer-page-not-logged";
+import Page404 from "../page-404/page-404";
 
 type AppScreenProps = {
   cardsCount: number;
@@ -20,6 +21,7 @@ function App({ cardsCount }: AppScreenProps): JSX.Element {
       <FavoritesPageEmpty />
       <MainScreenEmpty />
       <OfferPageNotLogged />
+      <Page404 />
     </>
   );
 }
